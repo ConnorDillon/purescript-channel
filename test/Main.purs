4cdc5be273e8691cc8a09ebce1994ccc14cdfe47
Connor Dillon <connor@cdillon.nl>
2021-04-16 17:18:24 +0200

    Added connect test case

--- test/Main.purs
+++ test/Main.purs
@@ -2,7 +2,7 @@ module Test.Main where
 
 import Prelude
 
-import Concurrent.Channel (newChannel, recv, send)
+import Concurrent.Channel (connect, newChannel, recv, send)
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Aff (Aff, launchAff_)
@@ -38,9 +38,21 @@ testCloseSend = test "close/send" do
   chan.close
   not <$> send chan.output 1
 
+testConnect :: Aff Unit
+testConnect = test "connect" do
+  chan1 <- liftEffect newChannel
+  chan2 <- liftEffect newChannel
+  liftEffect $ connect chan1.input chan2.output
+  void $ send chan1.output 1
+  void $ send chan1.output 2
+  result1 <- recv chan2.input
+  result2 <- recv chan2.input
+  pure $ result1 == Just 1 && result2 == Just 2
+
 main :: Effect Unit
 main = launchAff_ do
   testSend
   testSendRecv
   testCloseRecv
   testCloseSend
+  testConnect
